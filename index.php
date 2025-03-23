b0f70d6e68b78f0600cad3546a59f2c3c9d345c8
KauaHPedro <kauahcpedro@hotmail.com>
2025-03-23 20:44:59 -0300

    adicionando primeiras linhas de código

---
+++ index.php
@@ -0,0 +1,61 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200..700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <title>Curso PHP - Coder</title>
+</head>
+<body>
+    <header class="cabecalho">
+        <h1>Curso PHP</h1>
+        <h2>Índice dos Exercícios</h2>
+    </header>
+    <main class="conteudo">
+        <div class="exercicios">
+           <nav class="modulos">
+               <div class="modulo">
+                   <h3>Módulo 01 - Básico</h3>
+                   <ul>
+                       <li><a href="#">Exercício A</a></li>
+                       <li><a href="#">Exercício B</a></li>
+                       <li><a href="#">Exercício C</a></li>
+                   </ul>
+               </div>
+
+               <div class="modulo">
+                   <h3>Módulo 02 - Intermediário</h3>
+                   <ul>
+                       <li><a href="#">Exercício A</a></li>
+                       <li><a href="#">Exercício B</a></li>
+                       <li><a href="#">Exercício C</a></li>
+                   </ul>
+               </div>
+
+               <div class="modulo">
+                   <h3>Módulo 03 - Básico</h3>
+                   <ul>
+                       <li><a href="#">Exercício A</a></li>
+                       <li><a href="#">Exercício B</a></li>
+                       <li><a href="#">Exercício C</a></li>
+                   </ul>
+               </div>
+
+               <div class="modulo">
+                   <h3>Módulo 04 - Básico</h3>
+                   <ul>
+                       <li><a href="#">Exercício A</a></li>
+                       <li><a href="#">Exercício B</a></li>
+                       <li><a href="#">Exercício C</a></li>
+                   </ul>
+               </div>
+           </nav>
+        </div>
+    </main>
+    <footer class="rodape">
+        COD3R & ALUNOS © <?= date('Y'); ?>
+    </footer>
+</body>
+</html>
